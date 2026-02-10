c994fea6b7715855864b37bf915417714c5f975e
SmartDude18 <alexanderh1027@gmail.com>
2026-02-10 11:06:45 -0700

    config to map, and add speed cap

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,3 +1,4 @@
+using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -18,6 +19,8 @@ public class PlayerMove : MonoBehaviour
     [SerializeField]
     private float moveSpeed;
     [SerializeField]
+    private float maxSpeed;
+    [SerializeField]
     private float jumpHeight;
     [SerializeField]
     private float groundCheckDistance;
@@ -32,6 +35,7 @@ public class PlayerMove : MonoBehaviour
     void Start()
     {
         body = transform.GetComponent<Rigidbody>();
+        body.maxLinearVelocity = maxSpeed;
 
         moveAction = InputSystem.actions.FindAction("Move");
         jumpAction = InputSystem.actions.FindAction("Jump");
@@ -50,11 +54,18 @@ public class PlayerMove : MonoBehaviour
         dataSystem.PlayerMove(moveValue.magnitude > 0,moveValue.magnitude);
         dataSystem.PlayerGrounded(isGrounded);
 
-        //movement
         transform.rotation = Quaternion.Euler(transform.rotation.x, cam.transform.eulerAngles.y, transform.rotation.z);
-        body.AddForce(((transform.forward * moveValue.y) + (transform.right * moveValue.x))* moveSpeed, ForceMode.Force);
-
+        Vector3 movePower = ((transform.forward * moveValue.y) + (transform.right * moveValue.x)) * moveSpeed;
+        if(isGrounded)
+        {
+            //movePower = ((-getGround().transform.forward * moveValue.y) + (-getGround().transform.right * moveValue.x)) * moveSpeed;
+            movePower = movePower.ProjectOntoPlane(getGround().normal);
+        }
+        //movement
         
+        body.AddForce(movePower, ForceMode.Force);
+        
+
         //jump system
         //has jumped is used to make the movement off of 1 frame of jump force, making the jump more consistent
         //could be messed with in theory by pressing quickly while still in ground check range, but for how small it is... good luck
@@ -63,7 +74,7 @@ public class PlayerMove : MonoBehaviour
             hasJumped = true;
             jump();
         }
-        else if(!jumpAction.IsPressed() && isGrounded && hasJumped)
+        else if (!jumpAction.IsPressed() && isGrounded && hasJumped)
         {
             hasJumped = false;
         }
@@ -79,5 +90,12 @@ public class PlayerMove : MonoBehaviour
     {
         RaycastHit hit;
         return (Physics.Raycast(foot.transform.position, Vector3.down, out hit, groundCheckDistance, groundLayer));
+    }
+
+    private RaycastHit getGround()
+    {
+        RaycastHit hit;
+        Physics.Raycast(foot.transform.position, Vector3.down, out hit, groundCheckDistance, groundLayer);
+        return hit;
     }
 }
